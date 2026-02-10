489ed93dd79383ba13391daecf8e0a8f810e3f92
Roman Godz <romangodz@gmail.com>
2026-02-09 22:34:24 -0800

    fix: useMemo depends on allProperties, show all on load

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -52,7 +52,7 @@ export default function FeaturedProperties() {
 
       return true;
     });
-  }, [filters]);
+  }, [filters, allProperties]);
 
   const container = {
     hidden: { opacity: 0 },
